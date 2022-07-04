400bafc6580975dbda6a351f7b86f800332d5f9b
sedx876 <swatkins876@gmail.com>
2022-07-04 11:38:10 -0400

    Day1 and Day2

---
+++ Day1/app.py
@@ -0,0 +1,23 @@
+print('HelloWorld')
+print('Day 1 - Python Print Function')
+# \n Is used for line breaks
+print('Hello World!\nHello World!')
+# Concatenation use '+'
+print('Hello ' + 'Sharon')
+
+# INPUT FUNCTION
+input('What Is Your Name? ')
+print('Hello ' + input('What is your name? ') + '!')
+
+input('What Is Your Name? ')
+print(len(input('What is your name? ')))
+
+name = input('What is your name? ')
+print(name)
+
+print('Welcome To Band Name Generator!')
+city = input('What City Did You Grow Up In? \n')
+pet = input('What Is The Name Of Your Pet? \n')
+print("Your Band name is: " + city + ' ' + pet)
+
+
---
+++ Day2/app.py
@@ -0,0 +1,3 @@
+# Python Primitive Data Types
+# Data Types: String, Integer, Float, Boolean
+print('Hello'[-1])
