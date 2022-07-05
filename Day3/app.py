ae878a9422c3d6c03e3c47a742b513ccf6a1706e
sedx876 <swatkins876@gmail.com>
2022-07-05 08:56:17 -0400

    Day3

---
+++ Day3/app.py
@@ -0,0 +1,8 @@
+# Control Flow if/else statements and Conditional Operators
+print('Welcome to the RollerCoaster!!!')
+height = int(input('What is Your height in cm? '))
+
+if height > 120:
+    print('You can ride the rollercoaster!')
+else:
+    print('You CANNOT ride the rollercoaster')
